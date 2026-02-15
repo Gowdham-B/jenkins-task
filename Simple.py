08fcc61c4289ca45612819f7b2349decaf3e7082
Gowdham-B <gbrocks2005@gmail.com>
2026-02-15 18:49:38 +0530

    Add function to sum two numbers with user input

---
+++ Simple.py
@@ -0,0 +1,14 @@
+# Function definition
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Function call
+sum_result = add_numbers(num1, num2)
+
+# Output
+print("The sum is:", sum_result)
